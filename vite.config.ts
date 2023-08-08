aeec0c5127c5ff25fb6567c7b3e7b72b3adf5032
mohamadhassan <emohamadhassan@gmail.com>
2023-08-08 14:22:39 +0330

    Working on gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import rollupTsconfigPaths from "rollup-plugin-tsconfig-paths";
 
 export default defineConfig({
+  base: '/vue-web-otp/',
   plugins: [tsconfigPaths(), vue()],
   build: {
     lib: {
